ac7490cd1d5e12c3ffe48b78033abca2faca9e23
Daniel Mrózek <mrazicz@gmail.com>
2011-06-14 01:01:59 +0200

    powerdns_gdocs v0.1 RC1

---
+++ lib/models.rb
@@ -0,0 +1,117 @@
+# This file contains all models for datamapper
+class Domain
+  include DataMapper::Resource
+
+  property  :id,              Serial
+  property  :name,            String, :required => true, :unique => true, :length => 255
+  property  :master,          String, :length => 128
+  property  :last_check,      Integer
+  property  :type,            String, :required => true, :length => 10
+  property  :notified_serial, Integer
+  property  :account,         String, :length => 40
+
+  has n, :records
+  has n, :zones
+end
+
+class Zone
+  include DataMapper::Resource
+
+  property  :id,              Serial
+  property  :domain_id,       Integer,  :required => true
+  property  :owner,           Integer,  :required => true
+  property  :comment,         Text,     :default => "NULL"
+
+  belongs_to :domain
+end
+
+class Record
+  include DataMapper::Resource
+
+  property  :id,              Serial
+  property  :domain_id,       Integer, :key => true
+  property  :name,            String, :length => 255, :key => true
+  property  :type,            String, :length => 6
+  property  :content,         String, :length => 255
+  property  :ttl,             Integer
+  property  :prio,            Integer
+  property  :change_date,     Integer
+
+  belongs_to :domain
+end
+
+class User
+  include DataMapper::Resource
+
+  attr_accessor :id
+
+  property  :id,              Serial
+  property  :username,        String, :length => 16, :required => true
+  property  :password,        String, :length => 34, :required => true
+  property  :fullname,        String, :length => 255, :required => true
+  property  :email,           String, :length => 255, :required => true
+  property  :description,     Text, :required => true
+  property  :active,          Integer, :required => true
+  property  :perm_templ,      Integer, :required => true
+end
+
+class PermItem
+  include DataMapper::Resource
+
+  property  :id,              Serial
+  property  :name,            String, :length => 64, :required => true
+  property  :descr,           Text, :required => true
+end
+
+class PermTempl
+  include DataMapper::Resource
+
+  storage_names[:default] = "perm_templ"
+
+  property  :id,              Serial
+  property  :name,            String, :length => 128, :required => true
+  property  :descr,           Text, :required => true
+end
+
+class PermTemplItem
+  include DataMapper::Resource
+
+  property  :id,              Serial
+  property  :templ_id,        Integer, :required => true, :key => true
+  property  :perm_id,         Integer, :required => true, :key => true
+end
+
+class Supermaster
+  include DataMapper::Resource
+
+  property  :ip,              String, :length => 25, :required => true, :key => true
+  property  :nameserver,      String, :length => 255, :required => true
+  property  :account,         String, :length => 40
+end
+
+class ZoneTempl
+  include DataMapper::Resource
+
+  storage_names[:default] = "zone_templ"
+
+  property  :id,              Serial
+  property  :name,            String, :length => 128, :required => true
+  property  :descr,           String, :length => 1024, :required => true
+  property  :owner,           Integer, :required => true
+
+  has n, :zone_templ_records
+end
+
+class ZoneTemplRecord
+  include DataMapper::Resource
+  
+  property  :id,              Serial
+  property  :zone_templ_id,   Integer, :required => true, :key => true
+  property  :name,            String, :length => 255, :required => true
+  property  :type,            String, :length => 6, :required => true
+  property  :content,         String, :length => 255, :required => true
+  property  :ttl,             Integer, :required => true
+  property  :prio,            Integer, :required => true
+
+  belongs_to :zone_templ
+end
---
+++ lib/pdns_controller.rb
@@ -0,0 +1,128 @@
+class PdnsController
+  require "data_mapper"
+  require "models"
+  require "resolv"
+  
+  # constructor
+  def initialize(db, pdns)
+    DataMapper.setup(:default, {
+      :adapter  => db["adapter"],
+      :host     => db["host"],
+      :username => db["username"],
+      :password => db["password"],
+      :database => db["database"]
+    })    
+    
+    DataMapper.finalize
+    
+    @templates = ZoneTempl.all(:name => pdns["template"]).zone_templ_records
+  end
+  
+  # add new record to db
+  def add(record)
+    unless exist?(record[0])
+      domain = new_domain(record[0])
+      zone = new_zone(domain.id)
+      for t in @templates
+        new_record(t, domain, record)
+      end
+      return record
+
+     end
+
+    return nil
+  end
+
+  # delete record
+  def del(record)
+    if exist?(record[0])
+      domain = Domain.all(:name => record[0])
+
+      domain.records.destroy
+      domain.zones.destroy
+      domain.destroy
+    end
+  end
+
+  def check_reg(record)
+    if exist?(record[0])
+      r = Resolv::DNS.new
+      nservers = r.getresources(record[0], Resolv::DNS::Resource::IN::NS)
+      template_ns = @templates.all(:type => "NS")
+      
+      return false if template_ns.length != nservers.length
+
+      nservers.each do |ns|
+        return false unless template_ns.all(:content => /^#{Regexp.escape(ns.name.to_s)}\.?$/).any?
+      end
+    end
+    return true
+  end
+
+  private
+
+  # check if domain already exist
+  def exist?(domain)
+    Domain.all(:name => domain).any?
+  end
+
+  # create new record in Domain table
+  def new_domain(domain)
+    d = Domain.new
+    d.name = domain
+    d.type = "NATIVE"
+
+    d.save
+    return d
+  end
+
+  # create new record in Zone table
+  def new_zone(domain_id)
+    z = Zone.new
+    z.domain_id = domain_id
+    z.owner = User.all(:username => "admin").first.id
+
+    z.save
+    return z
+  end
+
+  # create new record in Record table
+  def new_record(template, domain, record)
+    r = Record.new
+
+    r.domain_id = domain.id
+    r.name = subst(domain, template.name)
+    
+    if template.type == "A" || template.type == "CNAME" || template.type == "AAAA"
+      r.name += "." unless template.name.empty?
+      r.name += domain.name
+    end
+    
+    r.type = template.type
+    
+    if template.type == "A" || template.type == "AAAA"
+      if record[1].any?
+        r.content = record[1]
+      else 
+        r.content = template.content
+        record[1] = template.content
+      end
+    else
+      r.content = subst(domain, template.content)
+    end
+
+    r.ttl = template.ttl
+    r.prio = template.prio
+    r.change_date = Time.now.to_i
+    
+    r.save
+    return r
+  end
+
+  # substitute [ZONE] and [SERIAL] and return it
+  def subst(domain, text)
+    stext = text.gsub /\[ZONE\]/, domain.name
+    stext.gsub! /\[SERIAL\]/, Time.now.strftime("%Y%m%d00")
+    return stext
+  end
+end
